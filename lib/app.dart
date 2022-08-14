7a0b754862d4a28f5ba309a17b25d5381b922675
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-08-14 21:49:53 +0200

    finished to lecture 121

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qoutes_app/core/utils/app_colors.dart';
+import 'package:qoutes_app/core/utils/app_strings.dart';
 import 'package:qoutes_app/features/random_qoute/presentation/screens/qoute_screens.dart';
 
 class QouteApp extends StatelessWidget {
@@ -8,18 +10,9 @@ class QouteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: AppStrings.appName,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: AppColors.primaryColor,
       ),
       home: const QouteScreen(),
     );
