702f7f70aca85b2ba454796eb4acfb470f34d3f4
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-08-14 21:41:32 +0200

    finished to lecture 11

---
+++ android/app/src/main/kotlin/com/answ/qoutes/qoutes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.answ.qoutes.qoutes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/random_qoute/presentation/screens/qoute_screens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QouteScreen extends StatelessWidget {
+  const QouteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Qoute Application is up and running')),
+    );
+  }
+}
