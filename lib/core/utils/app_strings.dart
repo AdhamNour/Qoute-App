8e9e985ff7ec350af9c41a7494807620ff830714
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-08-24 22:30:18 +0200

    finished data layer

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -2,4 +2,7 @@ class AppStrings {
   static const String appName = "Qoutes App";
   static const String fontFamily = "Tajawal";
   static const String cachedRandomQoute = "CACHED_RANDOM_QOUTES";
+  static const String url = 'http://quotes.stormconsultancy.co.uk/random.json';
+  static const String contentType = 'Content-Type';
+  static const String applicationJson = 'application/json';
 }
--- lib/features/random_qoute/data/datasources/random_quote_remote_data_source.dart
+++ lib/features/random_qoute/data/datasources/random_quote_remote_data_source.dart
@@ -1,5 +1,25 @@
+import 'package:qoutes_app/core/errors/exception.dart';
+import 'package:qoutes_app/core/utils/app_strings.dart';
 import 'package:qoutes_app/features/random_qoute/data/models/quote_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class RandomQouteRemoteDataSource {
   Future<QuoteModel> getRandomeQuote();
+}
+
+class RandomQouteRemoteDataSourceImpl extends RandomQouteRemoteDataSource {
+  final http.Client client;
+
+  RandomQouteRemoteDataSourceImpl({required this.client});
+  @override
+  Future<QuoteModel> getRandomeQuote() async {
+    final randomQouteUri = Uri.parse(AppStrings.url);
+    final reponse = await client
+        .get(randomQouteUri, headers: {'Content-Type': 'application/json'});
+    if (reponse.statusCode == 200) {
+      return QuoteModel.fromJson(reponse.body);
+    } else {
+      throw ServerException();
+    }
+  }
 }
